094ecde10f26ebd7f4b87b942c65473500f156dd
legstar67 <legstar6713@gmail.com>
2025-02-26 01:54:03 +0100

    the program works. it can be really upgraded

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # discordBot_TranferLink
+
+----------- GOAL -----------
+
+With your phone or another device you send to the bot a link. The bot which is running on your computer, will instantly open the web link on your navigator.
+
+----------- How -----------
+
+send JUST the link on the message (by default it opens only https link)
+
+for debug : you can send 'test' to know you tag and trusted users
+
+----------- SETUP -----------
+
+Bot to be used on Linux (tested only on ubuntu) with Firefox
+
+In bot.py modify the first two variables, it should look like something like :
+'''
+TOKEN_API_DISCORD = 'xxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxx.xxxxxxx'
+trusted_users = ["poutin93","Kimxx"]
+'''
+
+
+here is the command you have to execute on your terminal to setup the automatic lauching of the program at the start of your OS :
+
+
+you have to create a service :
+'''mkdir -p ~/.config/systemd/user'''
+'''sudo nano ~/.config/systemd/user/discordBot_TransferLink.service'''
+#you change the name 'discordBot_TransferLink'
+
+in the file you can copy paste this by changing the adresses :
+
+'''
+[Unit]
+Description=Bot discord to transfer link
+After=graphical.target
+
+[Service]
+ExecStart=ADRESS_OF_YOUR_PYTHON_INTERPRETER ADRESS_OF_THE_PYTHON_PROGRAM_bot.py
+WorkingDirectory=ADRESS_OF_THE_FOLDER_WHERE_THE_PYTHON_PROGRAM_bot.py_IS
+StandardOutput=inherit
+StandardError=inherit
+Environment=DISPLAY=:0
+Restart=always
+
+[Install]
+WantedBy=default.target
+
+[Service]
+ExecStartPre=/bin/sleep 10
+'''
+
+#'ExecStartPre=/bin/sleep 10' add a delay of 10sec for stating the service. it is to let the time to start the graphical session and to connect to internet , maybe to increase if you have very very slow computer
+
+
+Then, ~~authorize your session to have access to xhost
+'''xhost +SI:localuser:NAME_OF_YOUR_SESSION'''
+
+Next, as you modified a service
+'''sudo systemctl daemon-reload'''
+
+Next , you can enable for starting automatically when the OS launches:
+'''systemctl --user enable discordBot_TransferLink.service'''
+
+Or to test it you can start the service with this command : 
+'''systemctl --user start discordBot_TransferLink.service'''
+(and to stop is '''systemctl --user stop discordBot_TransferLink.service''')
+
+TIPS : to help you to debug potential bugs you can see some logs and if the service is running with 
+'''systemctl --user status discordBot_TransferLink.service'''
+
+
+
--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 import subprocess
 from dotenv import load_dotenv
 import os
+import time 
 
-# Remplacez 'os.getenv("TOKEN_API_DISCORD")' par le token de votre bot
-TOKEN_API_DISCORD = os.getenv("TOKEN_API_DISCORD")
-trusted_users = [os.getenv("TRUSTED_USER")]
 
-# Créez une instance de bot avec un préfixe de commande
+TOKEN_API_DISCORD = os.getenv("TOKEN_API_DISCORD") # Replace 'os.getenv("TOKEN_API_DISCORD")' by the token of your bot
+trusted_users = [os.getenv("TRUSTED_USER")] # Remplacez 'os.getenv("TRUSTED_USER")' by the tag of your discord account
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
@@ -17,27 +18,44 @@ intents.guild_messages = True
 client = discord.Client(intents=intents)
 #bot = commands.Bot(command_prefix='', intents=intents)
 
-# Événement lorsque le bot est prêt
+# Executed when the bot launches and is ready
 @client.event
 async def on_ready():
-    print(f'Bot connecté en tant que {client.user}')
+    print(f'Bot connected as {client.user}')
 
-# Commande simple pour répondre "pong" à "ping"
+# first command to test
 #@bot.command(name='ping')
-async def ping(ctx):
-    await ctx.send('pong \n')
-    await ctx.send("the user is '" + str(ctx.author) + "'")
+#async def ping(ctx):
+#    await ctx.send('pong \n')
+#    await ctx.send("the user is '" + str(ctx.author) + "'")
 
+
+############# for debuging
+#import logging
+
+#logging.basicConfig(level=logging.INFO)
+#logger = logging.getLogger(__name__)
+#################
 
 # Open the link with firefox
 @client.event
 async def on_message(message):
+    logger.info(f"Message reçu: {message.content}") ####### TODO for debuging
     if(message.content.lower().replace(" ", "").startswith("http")):
         if (str(message.author) in trusted_users):
             if(message.content.lower().replace(" ", "").startswith("https")):
-                await message.channel.send("Link accepted, it will be opened on your computer")
-                command = "firefox " + message.content
-                subprocess.check_output(command, shell=True, text=True)
+                #try:
+                    await message.channel.send("Link accepted, it will be opened on your computer")
+                    command = "firefox " + message.content
+                    subprocess.Popen(command, shell=True)
+                    #result = subprocess.run(command, shell=True, text=True, capture_output=True)
+                    #logger.info(f"Command output: {result.stdout}")
+                    #if result.stderr:
+                    #    logger.error(f"Command error: {result.stderr}")
+                #except subprocess.CalledProcessError as e:
+                #    await message.channel.send(f"Error by opening the link : {e}")
+                    await message.channel.send("Link Opened !")
+                
             else:
                 await message.channel.send("Link refused")
 
@@ -49,5 +67,7 @@ async def on_message(message):
 
 
 
-# Lancez le bot avec le token
-client.run(TOKEN_API_DISCORD)
+# Start the bot with the token
+if __name__ == "__main__":
+    #time.sleep(10)
+    client.run(TOKEN_API_DISCORD)
